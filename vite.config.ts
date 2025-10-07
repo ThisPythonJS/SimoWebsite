d38e38273867989ea021d449a6d50324f64efe78
ThisPythonJS <thispythonjs@gmail.com>
2025-10-07 14:22:04 -0300

    fix: correction and insertion of allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 80,
-        proxy: {
-            "/api": {
-                target: "http://localhost:80",
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-            },
-        },
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    proxy: {
+      "/api": {
+        target: "http://localhost:80",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
     },
-    plugins: [react()],
+  },
+  preview: {
+    allowedHosts: ["simo.squareweb.app"],
+  },
+  plugins: [react()],
 });
