916ebc1beda4a5b7f808435cc87725e964f2ca2c
ThisPythonJS <thispythonjs@gmail.com>
2025-10-07 01:08:22 -0300

    fix: adapted for square cloud host and port

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
     server: {
-        // host: '0.0.0.0',
-        // port: 80,
+        host: '0.0.0.0',
+        port: 80,
         proxy: {
             "/api": {
                 target: "http://localhost:80",
