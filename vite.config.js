674adfb842524c4d89889136e668c51132cff475
ThisPythonJS <thispythonjs@gmail.com>
2025-10-07 15:08:43 -0300

    fix: added proxy in preview

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,25 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: 80,
     proxy: {
       "/api": {
-        target: "http://api-simo.squareweb.app/api",
+        target: "https://api-simo.squareweb.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
         ws: true,
       },
     },
   },
   preview: {
-    allowedHosts: ["simo.squareweb.app", "api-simo.squareweb.app"],
+    allowedHosts: ["simo.squareweb.app"],
+    proxy: {
+      "/api": {
+        target: "https://api-simo.squareweb.app",
+        changeOrigin: true,
+        secure: true,
+        ws: true,
+      },
+    },
   },
   plugins: [react()],
 });
